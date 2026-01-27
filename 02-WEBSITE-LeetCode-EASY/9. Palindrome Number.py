a18822896b524015d97a7e3d215e4d54c0725217
Mohammod Tareq Aziz <mohammodtareqaziz@gmail.com>
2026-01-27 12:51:19 +0600

    Update 9. Palindrome Number.py

--- 02-WEBSITE-LeetCode-EASY/9. Palindrome Number.py
+++ 02-WEBSITE-LeetCode-EASY/9. Palindrome Number.py
@@ -24,3 +24,27 @@ class Solution:
 # using Numerical approach
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x < 0 : 
+            return False
+
+        divVal = 1
+        while x >= 10 * divVal:
+            divVal *= 10
+
+        while x > 0:
+            lDigit = x // divVal
+            rDigit = x % 10
+
+            if  lDigit != rDigit: 
+                return False
+
+            # lChop = x % divVal
+            # rChop = lChop // 10
+            # x = rChop
+            x = (x % divVal) // 10
+
+            divVal = divVal // 100
+        return True
+    
+# // is called Floor Division (or Integer Division)
+# 10 // 3 = 3
