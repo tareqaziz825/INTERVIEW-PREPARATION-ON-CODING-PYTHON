32fd7f376bf1755322a6d75f02f33aba8048e0e7
Mohammod Tareq Aziz <mohammodtareqaziz@gmail.com>
2026-01-27 10:23:13 +0600

    Update 9. Palindrome Number.py

--- 02-WEBSITE-LeetCode-EASY/9. Palindrome Number.py
+++ 02-WEBSITE-LeetCode-EASY/9. Palindrome Number.py
@@ -1,5 +1,26 @@
 # https://leetcode.com/problems/palindrome-number/
 
+# using String approach
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
         
+        strr = str(x)
+
+        # revStr = ""
+        # for char in strr:
+        #     revStr = char + revStr
+
+        # if strr == revStr:
+        #     return True
+        
+        # string[start : stop : step]
+        # [::-1] Reverses a String
+        # if strr == strr[::-1]:
+        #     return True
+        return strr == strr[::-1]
+
+# using Numerical approach
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
